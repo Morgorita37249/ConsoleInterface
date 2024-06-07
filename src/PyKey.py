91f2fc366bb6a8b28dc44b7f7c21880247f75a7f
Morgorita37249 <margaritapushkina37249@gmail.com>
2024-06-07 19:24:29 +0500

    cli

---
+++ src/PyKey.py
@@ -0,0 +1,124 @@
+import sys
+from shutil import get_terminal_size
+from os import system
+from pynput import keyboard
+
+restrictions = get_terminal_size()
+
+
+def print_there(x, y, text):
+    global cursor_left
+    global cursor_top
+    sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+    sys.stdout.write("\x1b7\x1b[%d;%dH" % (cursor_left, cursor_top))
+    sys.stdout.flush()
+
+
+def print_info(text):
+    global restrictions
+    print_there(restrictions.lines - 1, 0, text)
+
+
+cursor_left = 0
+cursor_top = 0
+
+
+def refresh_screen():
+    global cursor_left
+    global cursor_top
+    global buffer
+    to_print = buffer + "[EOF]"
+    print_there(0, 0, to_print)
+    sys.stdout.write("\x1b7\x1b[%d;%dH" % (cursor_top, cursor_left))
+    sys.stdout.flush()
+
+
+def on_press(key):
+    global cursor_left
+    global cursor_top
+    global restrictions
+    if key == keyboard.Key.right:
+        cursor_left = cursor_left + 1
+        if cursor_left >= restrictions.columns:
+            cursor_left = restrictions.columns - 1
+
+    if key == keyboard.Key.left:
+        cursor_left = cursor_left - 1
+        if cursor_left < 0:
+            cursor_left = 0
+
+    if key == keyboard.Key.down:
+        cursor_top = cursor_top + 1
+        if cursor_top >= restrictions.lines:
+            cursor_top = restrictions.lines - 1
+
+    if key == keyboard.Key.up:
+        cursor_top = cursor_top - 1
+        if cursor_top < 0:
+            cursor_top = 0
+
+    if key == keyboard.Key.space:
+        update_buffer(" ")
+    if key == keyboard.Key.f2:
+        save_files()
+        return
+
+    try:
+        just_symbol = key.char
+        update_buffer(key.char)
+
+    except AttributeError:
+        print_info(key)
+    refresh_screen()
+
+
+def save_files():
+    global buffer
+    try:
+        file = open(sys.argv[1], "w")
+        file.write(buffer)
+        file.close()
+        print_info("file {0} saved successfully".format(sys.argv[1]))
+    except Exception as e:
+        print_there(restrictions.lines - 1, 0, repr(e))
+
+
+def spaces(times):
+    s = ''
+    for i in range(times):
+        s += ' '
+    return s
+
+
+def update_buffer(char):
+    global buffer
+    global cursor_left
+    global cursor_top
+    global restrictions
+    buffer_shift = restrictions.columns * cursor_top + cursor_left
+    if buffer_shift <= len(buffer):
+        buffer = buffer[:buffer_shift] + char + buffer[buffer_shift:]
+
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+
+n = len(sys.argv)
+print("Total arguments passed:", n)
+if n < 2:
+    print("usage: py pykey.py [file to edit]")
+    sys.exit()
+
+f = open(sys.argv[1], "r")
+system('cls')
+buffer = f.read()
+f.close()
+
+print_there(0, 0, buffer)
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
